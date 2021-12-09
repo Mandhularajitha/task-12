91c942df9c29145c7fde8747ffdb579b0dfd005c
Mandhularajitha <91309820+Mandhularajitha@users.noreply.github.com>
2021-12-09 23:27:24 +0530

    Add files via upload

---
+++ quize.js
@@ -0,0 +1,24 @@
+const quizForm = document.querySelector(".quize-from");
+const submitAnswerBtn = document.querySelector("#submit-answer-btn")
+const outputE1 = document.querySelector("#output");
+
+const correctAnswers = ["90°","right angle","0","no","100°"];
+
+function calculatescore() {
+    let score = 0;
+    let index = 0;
+    const formResults = new FormData(quizForm);
+    for(let value of formResults.values()){
+        if(value === correctAnswers[index]){
+            score=score + 1;
+            // console.log(score)
+        }
+        index=index+1;
+        // console.log(index)
+    }
+    // console.log(score);
+    outputE1.innerText = "your score is"+ score;
+
+}
+
+submitAnswerBtn.addEventListener('click',calculatescore);
